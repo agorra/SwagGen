e076a25f800938815df0941117bd850ed7485154
Ruslan Kavetsky <rusik@hey.com>
2021-06-11 16:14:41 +0300

    Update SHA for Brew

--- Formula/SwagGen.rb
+++ Formula/SwagGen.rb
@@ -2,7 +2,7 @@ class Swaggen < Formula
   desc "Swagger/OpenAPISpec code generator written in Swift"
   homepage "https://github.com/agorra/SwagGen"
   url "https://github.com/agorra/SwagGen/archive/5.0.2.tar.gz"
-  sha256 "9e490a7e026a47f0bcb872ef61f5d5cdcd3b4ffc37968bcde7a6819d236f766b"
+  sha256 "48c99f003ab0ccda1884c677eddd4ce10877d57db2f619863eea15fb59131107"
   head "https://github.com/agorra/SwagGen.git"
 
   depends_on :xcode
